034ac973356ebc6033d7597cac9b8f40ab3756dc
spacehaz <spacehaz@gmail.com>
2023-02-17 08:34:30 +0100

    added update for rainbow

--- src/components/pages/claim-page/choose-wallet/index.tsx
+++ src/components/pages/claim-page/choose-wallet/index.tsx
@@ -50,8 +50,8 @@ const ChooseWallet: FC<ReduxType> = () => {
         nonce: generateNonce(),
       })
       .then(({ uri }) => {
-        alert(`zerion://wc?uri=${uri}`)
-        window.location.href = `zerion://wc?uri=${uri}`
+        alert(`rainbow://wc?uri=${uri}`)
+        window.location.href = `rainbow://wc?uri=${uri}`
       })
   }, [client])
 
