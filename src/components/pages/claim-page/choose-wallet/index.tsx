99f6971c284e35e6c66c08f15df124a551989d45
spacehaz <spacehaz@gmail.com>
2023-02-18 09:09:17 +0100

    added update for link to wallet

--- src/components/pages/claim-page/choose-wallet/index.tsx
+++ src/components/pages/claim-page/choose-wallet/index.tsx
@@ -68,9 +68,10 @@ const ChooseWallet: FC<ReduxType> = () => {
     if (!client) { return }
     client
       .request({
-        aud: window.location.href,
+        aud: 'https://jazzy-donut-086baa.netlify.app',
         domain: window.location.hostname.split(".").slice(-2).join("."),
         chainId: "eip155:1",
+        type: "eip4361",
         nonce: generateNonce(),
       })
       .then(({ uri }) => {
