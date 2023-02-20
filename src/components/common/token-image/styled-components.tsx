e73a0bec976fafc5c488d1d6f858d69592bcac0d
spacehaz <spacehaz@gmail.com>
2023-02-20 16:02:16 +0100

    added updates for video

--- src/components/common/token-image/styled-components.tsx
+++ src/components/common/token-image/styled-components.tsx
@@ -9,7 +9,21 @@ export const TokenImage = styled.img<TProps>`
   width: 184px;
   height: 184px;
   object-fit: cover;
-  display: block;
+  display: block; 
   border-radius: 8px;
   overflow: hidden;
+`
+
+export const TokenVideo = styled.video`
+  width: 184px;
+  height: 184px;
+  object-fit: cover;
+  display: block; 
+  border-radius: 8px;
+  margin-left: auto;
+  margin-right: auto;
+`
+
+export const TokenVideoSource = styled.source`
+  
 `
--- src/components/pages/claim-page/initial-screen/index.tsx
+++ src/components/pages/claim-page/initial-screen/index.tsx
@@ -78,6 +78,7 @@ const InitialScreen: FC<ReduxType> = ({
       appearance={loading ? 'inverted' : 'default'}
       title='Add to my collection'
       onClick={() => {
+        return false
         if (type === 'erc1155') {
           return claimERC1155()
         }
