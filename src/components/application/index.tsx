9eab790fc376f2455559227c7ce30b4cafd3302d
spacehaz <spacehaz@gmail.com>
2024-11-06 18:39:13 +0300

    updates for zero network

--- src/components/application/index.tsx
+++ src/components/application/index.tsx
@@ -5,8 +5,27 @@ import { Container } from './styled-components'
 import { queryClient, config } from './connectors/wallet-connect'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { Loader } from 'components/common'
 
 function Application () {
+  const [
+    init,
+    setInit
+  ] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (window.location.href.includes('/#/')) {
+      window.location.href = window.location.href.replace('/#/', '/')
+      return
+    }
+    setInit(true)
+  }, [])
+
+  if (!init) {
+    return <Loader />
+  }
+
   return <WagmiProvider config={config}>
     <QueryClientProvider client={queryClient}>
       <Container>
